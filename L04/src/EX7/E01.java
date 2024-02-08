69564bdcb8864ed0343aeb8c17a112e2991d0580
nilshilmarn <nilshilmarn@gmail.com>
2024-02-08 13:24:21 +0100

    Update to L04 EX7

--- L04/src/EX7/E01.java
+++ L04/src/EX7/E01.java
@@ -16,27 +16,57 @@ public class E01 {
 
         int age = input.nextInt();
 
-        System.out.println(team(isBoy, age));
+        System.out.printf("Institution: %s \n", institution(age));
+        System.out.printf("Team: %s", team(isBoy, age));
 
     }
 
+    public static String institution(int age)
+    {
+
+        String institution = "";
+
+        if (age == 0)
+        {
+            institution = "Home";
+        }
+        else if (age == 1 || age == 2)
+        {
+            institution = "Nursery";
+        }
+        else if (age >= 3 && age <= 5 )
+        {
+            institution = "Kindergarten";
+        }
+        else if (age >= 6 && age <= 16)
+        {
+            institution = "School";
+        }
+        else
+        {
+            institution = "Out of school";
+        }
+
+        return institution;
+
+    }
 
     public static String team(boolean isBoy, int age)
     {
         String team = "";
 
         if (isBoy)
+        {
             if (age < 8)
             {
                 team = "Young cubs";
-
             }
             else
             {
                 team = "Cool boys";
-
             }
-            else
+        }
+        else
          {
             if (age < 8)
             {
