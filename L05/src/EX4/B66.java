f06160848d0c2f3c8206441bedc37d4ec94c1313
nilshilmarn <nilshilmarn@gmail.com>
2024-02-10 23:08:55 +0100

    final touches for L05 exercise 4 B66

--- L05/src/EX4/B66.java
+++ L05/src/EX4/B66.java
@@ -1,20 +1,27 @@
 package EX4;
 
+import java.util.Scanner;
 public class B66
 {
 
     public static void main(String[] args)
     {
-        displayPattern();
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+
+        int n = input.nextInt();
+
+        displayPattern(n);
     }
 
 
-    public static void displayPattern()
+    public static void displayPattern(int n)
     {
 
 
         int i = 1;
-        int n = 3;
         int spaces = n - 1; // if n = 3 -> 4, 2, 0 <-> (3-2) * 2 = 4, (2 - 1) * 2 = 2, (1-1) * 2 = 0
 
         while (i <= n)
@@ -22,13 +29,14 @@ public class B66
 
             int k = 0; // Reset k to 0
 
+            // Comment this while loop out to make the pattern right-aligned
             while (k < spaces)
             {
                 System.out.print("  ");
                 k++;
             }
 
-            int j = 1; // Reset j to 1.
+            int j = 1; // Reset j to 1 to print 1 -> n
 
             while (j - 1 < i)
             {
